5149d796930a97f1fd001304c08d6feffb9226e9
Vüsal Quliyev <142631679+vusalgul1yevv@users.noreply.github.com>
2025-12-05 03:15:44 +0400

    Update project

--- src/main/java/com/example/Start_Bootstrap_Blog/controller/HomeController.java
+++ src/main/java/com/example/Start_Bootstrap_Blog/controller/HomeController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home(){
-        return "index.html";
+        return "index";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "dashboard/index";
     }
 }
